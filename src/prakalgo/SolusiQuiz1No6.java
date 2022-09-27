26e05140071c43b2f7e2e42980982a87c5f01c91
Cent <Cent@DESKTOP-CM1KUG1>
2022-09-27 17:57:09 +0700

    update vincent

---
+++ src/prakalgo/SolusiQuiz1No6.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prakalgo;
+
+/**
+ *
+ * @author Cent
+ */
+public class SolusiQuiz1No6 {
+    public static void main(String[] args) {
+        int number = 7;
+        char ch = '*';
+        for (int i = 0; i < number; i++) {
+            for (int j = 0; j < number; j++) {
+                System.out.print(ch);
+                if(ch == '*'){
+                    ch = ' ';
+                }else{
+                    ch = '*';
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
